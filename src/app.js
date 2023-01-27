c4f02666e03c361054c52a0fc7e4b1b68f8d3c16
DrUnicornIT <congcongcong6e@gmail.com>
2023-01-28 06:42:46 +0700

    P - Add new prf, proj

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom"
-
+import { productInputs, userInputs } from "./formSource";
 function App() {
   return (
     <div className="App">
@@ -21,13 +21,15 @@ function App() {
             <Route path="users">
               <Route index element={<List/>}/>
               <Route path=":userID" element={<Single/>}/>
-              <Route path="new" element={<New/>}/>
+              <Route 
+                path="new" element={<New inputs={userInputs} title="Add New User"/>}/>
             </Route>
 
             <Route path="products">
               <Route index element={<List/>}/>
               <Route path=":productsID" element={<Single/>}/>
-              <Route path="new" element={<New/>}/>
+              <Route 
+                path="new" element={<New inputs={productInputs} title="Add New Product"/>}/>
             </Route>
 
           </Route>
