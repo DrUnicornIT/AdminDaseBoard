b6aea70fc4a146916e7c741caa3f86f676edecdb
DrUnicornIT <congcongcong6e@gmail.com>
2023-01-28 04:31:26 +0700

    P - Widget

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import "./Home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget"
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
@@ -14,6 +16,10 @@ const Home = () => {
           <Widget type="order"/>
           <Widget type="earning"/>
           <Widget type="balance"/>
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart aspect={2/1} title="Last 6 Months (Revenue)" />
         </div>
       </div>
     </div>
